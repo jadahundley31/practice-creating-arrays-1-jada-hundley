7b40e9b7cbc0a65cf900e75da689e10a70d718ea
Jada Hundley <jadahundley@gmail.com>
2026-05-03 16:28:53 -0500

    completed practice creating arrays.

---
+++ main.js
@@ -0,0 +1,17 @@
+//Task 1: Create Arrays
+//An array of 5 grocery strore items.
+let groceryStoreItems = ["eggs", "milk", "cereal", "bread", "bacon"];
+
+//An array of 3 numbers representing prices.
+let prices = [5.00, 10.00, 15.00];
+
+//Task 2: Access and Log the Values
+console.log(groceryStoreItems);
+console.log(prices);
+//log 3 individual items
+console.log(groceryStoreItems[4]);
+console.log(groceryStoreItems[2]);
+console.log(groceryStoreItems[0]);
+//log 2 prices
+console.log(prices[1]);
+console.log(prices[2]);
